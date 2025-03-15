70c5eed0b1c20ed7a515f639e40672dcfd37dba8
goyle0 <f.syou0909@gmail.com>
2025-03-15 21:14:23 +0900

    各画面に遷移されるように修正

--- nurse-app/store/index.ts
+++ nurse-app/store/index.ts
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import treatmentReducer from './slices/treatmentSlice';
+import injectionReducer from './slices/injectionSlice';
+import nursingRecordReducer from './slices/nursingRecordSlice';
+import nursingPlanReducer from './slices/nursingPlanSlice';
 
 export const store = configureStore({
   reducer: {
     treatment: treatmentReducer,
+    injection: injectionReducer,
+    nursingRecord: nursingRecordReducer,
+    nursingPlan: nursingPlanReducer,
   },
 });
 
